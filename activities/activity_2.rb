1d330d6bb495902bc24cd3837d11b02e7b6a8f87
janninegopuansuy <janninegopuansuy@gmail.com>
2023-02-23 14:30:21 +0800

    answered activity2

--- activities/activity_2.rb
+++ activities/activity_2.rb
@@ -3,3 +3,12 @@
 #  Values will be your respective names.
 #  Write a function that takes a Hash and returns the full name.
 #  Print on console the result.
+
+def fullName(my_name)
+  full_name = ""
+  my_name.each do |key, value|
+    full_name = full_name + value + " "
+  end
+  return full_name
+end
+p fullName(Hash[first_name:'Jannine Daphne', middle_name: 'Yap', last_name: 'Gopuansuy'])
